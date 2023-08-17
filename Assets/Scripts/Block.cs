05d96a690cf84d48444751bfbf7e654423e8acc5
Daniel Kierkegaard Andersen <dax@daxode.dk>
2023-08-17 14:16:31 +0200

    Dictionary removals

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block
+public struct Block
 {
     public Tile top, side, bottom;
-
     public TilePos topPos, sidePos, bottomPos;
 
     public Block(Tile tile)
     {
         top = side = bottom = tile;
-        GetPositions();
+        
+        topPos = TilePos.tiles[top];
+        sidePos = TilePos.tiles[side];
+        bottomPos = TilePos.tiles[bottom];
     }
 
     public Block(Tile top, Tile side, Tile bottom)
@@ -19,24 +22,25 @@ public class Block
         this.top = top;
         this.side = side;
         this.bottom = bottom;
-        GetPositions();
-    }
-
-    void GetPositions()
-    {
+        
         topPos = TilePos.tiles[top];
         sidePos = TilePos.tiles[side];
         bottomPos = TilePos.tiles[bottom];
     }
 
-
-    public static Dictionary<BlockType, Block> blocks = new Dictionary<BlockType, Block>(){
-        {BlockType.Grass, new Block(Tile.Grass, Tile.GrassSide, Tile.Dirt)},
-        {BlockType.Dirt, new Block(Tile.Dirt)},
-        {BlockType.Stone, new Block(Tile.Stone)},
-        {BlockType.Trunk, new Block(Tile.TreeCX, Tile.TreeSide, Tile.TreeCX)},
-        {BlockType.Leaves, new Block(Tile.Leaves)},
-    };
+    public static BlockDict blocks => new ();
+    public struct BlockDict
+    {
+        public Block this[BlockType key] => key switch
+        {
+            BlockType.Dirt => new Block(Tile.Dirt),
+            BlockType.Grass => new Block(Tile.Grass, Tile.GrassSide, Tile.Dirt),
+            BlockType.Stone => new Block(Tile.Stone),
+            BlockType.Trunk => new Block(Tile.TreeCX, Tile.TreeSide, Tile.TreeCX),
+            BlockType.Leaves => new Block(Tile.Leaves)
+        };
+    }
 }
+
 
 public enum BlockType {Air, Dirt, Grass, Stone, Trunk, Leaves}
--- Assets/Scripts/TilePos.cs
+++ Assets/Scripts/TilePos.cs
@@ -21,16 +21,22 @@ public struct TilePos
         uv3 = new Vector2((xPos + 1) / 16f - .001f, yPos / 16f + .001f);
     }
 
-    public static Dictionary<Tile, TilePos> tiles = new Dictionary<Tile, TilePos>()
+    public static TileDict tiles => new();
+
+    public struct TileDict
     {
-        {Tile.Dirt, new TilePos(0,0)},
-        {Tile.Grass, new TilePos(1,0)},
-        {Tile.GrassSide, new TilePos(0,1)},
-        {Tile.Stone, new TilePos(0,2)},
-        {Tile.TreeSide, new TilePos(0,4)},
-        {Tile.TreeCX, new TilePos(0,3)},
-        {Tile.Leaves, new TilePos(0,5)},
-    };
+        public TilePos this[Tile key] => key switch
+        {
+            Tile.Dirt => new TilePos(0,0),
+            Tile.Grass => new TilePos(1,0),
+            Tile.GrassSide => new TilePos(0,1),
+            Tile.Stone => new TilePos(0,2),
+            Tile.TreeSide => new TilePos(0,4),
+            Tile.TreeCX => new TilePos(0,3),
+            Tile.Leaves => new TilePos(0,5),
+            _ => new TilePos(0,0)
+        };
+    }
 }
 
 public enum Tile {Dirt, Grass, GrassSide, Stone, TreeSide, TreeCX, Leaves}
